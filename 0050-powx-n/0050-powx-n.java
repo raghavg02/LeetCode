d5fb80242ee630a828134e6436e296c27bf91e62
Raghav Gupta <119950517+raghavg02@users.noreply.github.com>
2026-07-26 15:36:35 +0530

    Time: 0 ms (100%), Space: 47.6 MB (85.68%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        while (N > 0) {
+            if ((N & 1) == 1) {
+                ans = ans * x;
+            }
+            x = x * x;
+            N = N >> 1;
+        }
+        return ans;
+
+    }
+}
