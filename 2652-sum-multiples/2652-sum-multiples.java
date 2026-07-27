50f55ae63fd582bbfd07c8368b255bd3a281492c
Raghav Gupta <119950517+raghavg02@users.noreply.github.com>
2026-07-27 12:04:29 +0530

    Time: 2 ms (98.23%), Space: 42.9 MB (8.92%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -3,7 +3,7 @@ class Solution {
         int sum=0;
         for(int i=3;i<=n;i++){
             if(i%3==0||i%5==0||i%7==0){
-                sum+=i;
+                sum+= i;
             }
         }
         return sum;
